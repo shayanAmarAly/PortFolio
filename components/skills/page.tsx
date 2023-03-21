fa6bdd1cefd525a6124b065084ee6028a586fc8f
shayanAmarAly <101214599+shayanAmarAly@users.noreply.github.com>
2023-03-21 13:35:55 -0700

    URL change in line #05

--- components/skills/page.tsx
+++ components/skills/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import JS from "../../public/JS.gif"
-import Next from "../../public/nextGif.gif"
+import Next from "../../public/nextGIF.gif"
 import tail from "../../public/tail.gif"
 
 const Skills = () => {
